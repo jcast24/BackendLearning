f6031ada6627f805553d99e4d4a4f1e040351abd
Jesus Castro <castrojesus39@gmail.com>
2024-01-08 23:08:25 -0800

    moved routes into dedicated folder src/routes/

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import models from "../src/models/models.js";
+import routes from "./routes";
 
 const app = express();
 
@@ -18,57 +19,15 @@ app.use((req, res, next) => {
     models,
     me: models.users[1], // authenticated user 
   };
-  console.log(req.context);
+  // console.log(req.context);
   next();
 });
 
-app.get("/session", (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-})
-
-// Routes
-// All users
-app.get("/users", (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-// Single Users
-app.get("/users/:userId", (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get("/messages", (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post("/messages", (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id, 
-  };
-  
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete("/messages/:messageId", (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-  return res.send(message);
-});
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-});
+app.listen(process.env.port, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
